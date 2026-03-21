1a0441fa219fb50854b09bf76b2b10238ade25f1
Emanuelle <emanuellenmoura@gmail.com>
2026-03-21 19:07:57 -0300

    separação da base entre treino e teste e treinamento de modelo de regressão simples com aluguel e metragem na base de treino

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,5 @@
 from utils.pre_processing import *
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("data/aluguel.csv", delimiter=';')
 print(df.info())  #apenas variáveis numéricas
@@ -8,7 +9,7 @@ print(df.describe().T)
 '''Valor_Aluguel, Valor_Condominio e Metragem, talvez outliers, pois Q3 muito diferente do valor máximo
 variáveis quantitativas (quartos, banheiros, suítes e vagas) com máximos altos, 
 mas faz sentido para imóveis de alto padrão'''
-
+'''
 sns.histplot(df['Valor_Aluguel'], bins=30)
 plt.title("Distribuição de preço do aluguel")
 plt.show()
@@ -23,7 +24,7 @@ sns.histplot(df['Metragem'], bins=30)
 plt.title("Distribuição de metragem do imóvel")
 plt.show()
 #cauda longa também, distorção, mas menos que as outras
-
+'''
 '''tentei tratamento com capping pelo IQR, mas fez um teto artificial e para não remover dados usei transformação logarítmica
 para encolher os outliers, não enviesar o modelo'''
 data.apply_log(['Valor_Condominio', 'Valor_Aluguel', 'Metragem'])
@@ -54,3 +55,35 @@ plt.ylabel("Valor_Aluguel")
 plt.show()
 '''o aumento da metragem explica o aumento do aluguel, nuvem de pontos de largura relativamente constante, erro de 
 previsão similar para imóveis pequenos e grandes.'''
+
+data.heatmap()
+'''
+variáveis com correlação mais forte para aluguel:
+    Metragem(0.73) mais forte, o tamanho do imóvel é o fator que mais explica o preço
+    N_Vagas (0.66) o número de vagas pode ser um indicador de imóveis mais caros
+    N_banheiros e N_Suites (0.61), mesmo peso (obs: N_banheiros e N_Suites 0.92 entre si)
+'''
+
+data.separar_base('Valor_Aluguel')
+
+X = data.X_train[['Metragem']]  # Variável independente (características)
+y = data.y_train  # Variável dependente (rótulo)
+regressao_simples = LinearRegression()
+regressao_simples.fit(X, y)
+
+#intercepto ($\beta_0$) é onde a reta cruza o eixo Y (valor quando a metragem é 0)
+#coeficiente ($\beta_1$) indica quanto o aluguel aumenta para cada metro quadrado adicional
+#reta: $y = \beta_1*x + \beta_0$
+intercepto = regressao_simples.intercept_
+coeficiente = regressao_simples.coef_[0]
+print(f"Intercepto: {intercepto:.2f}")
+print(f"Coeficiente: {coeficiente:.2f}")
+print(f"\nEquação da Reta:")
+print(f"Valor_Aluguel = {intercepto:.2f} + ({coeficiente:.2f} * Metragem)")
+
+r2_treino = regressao_simples.score(X, y)
+print(f"R² do modelo de treinamento: {r2_treino:.4f}")
+'''
+O R² de ~0.52 indica que a metragem sozinha explica cerca de 52% da variação do aluguel, é um resultado ok, mas mostra 
+que o aluguel é influenciado por muitos outros fatores
+'''
