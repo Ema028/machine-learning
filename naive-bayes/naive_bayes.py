fc76f11a6ffba84efd2409b1f842b52979dc82fa
Emanuelle <emanuellenmoura@gmail.com>
2026-03-24 22:27:31 -0300

    verificação por validação cruzada

--- naive-bayes/naive_bayes.py
+++ naive-bayes/naive_bayes.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
+from sklearn.model_selection import cross_val_score
 from utils.pre_processing import verificar_base
 
 '''
@@ -35,7 +37,7 @@ conf_matrix = confusion_matrix(y_treino, pred_train)
 class_names = ['Crédito não alto', 'Crédito alto']
 plt.figure(figsize=(8, 6))
 sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=class_names, yticklabels=class_names)
-plt.title('Matriz de Confusão', pad=15)
+plt.title('Matriz de Confusão (base de treino)', pad=15)
 plt.ylabel('Valor Real', fontweight='bold')
 plt.xlabel('Previsões', fontweight='bold')
 plt.show()
@@ -46,18 +48,43 @@ recall = recall_score(y_teste, pred_test, average='macro')
 
 #em porcentagem
 print(f"\nAcurácia: {acuracia * 100:.2f}%")
-print(f"Recall: {recall * 100:.2f}%")
+print(f"Recall: {recall * 100:.2f}%\n\n")
 
 conf_matrix = confusion_matrix(y_teste, pred_test)
 class_names = ['Crédito não alto', 'Crédito alto']
 plt.figure(figsize=(8, 6))
 sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=class_names, yticklabels=class_names)
-plt.title('Matriz de Confusão', pad=15)
+plt.title('Matriz de Confusão (base de teste)', pad=15)
 plt.ylabel('Valor Real', fontweight='bold')
 plt.xlabel('Previsões', fontweight='bold')
 plt.show()
 '''comparação treino e teste:
-99.44% de acurácia no treino e 100% no teste, como a performance no teste foi superior ao treino,
+98.33% de acurácia no treino e 96.97% no teste, como a performance no teste foi superior ao treino,
 os números altos não foram por causa de overfitting, no início cogitei que o modelo poderia olhar Credit Score_Low
 e ter o gabarito, mas mesmo tirando essa coluna de X_teste e X_treino o desmpenho teve os mesmos números,
 acredito que a pontuação perfeita seja pela previsibilidade e tamanho reduzido do dataset'''
+
+'''resultados muito próximos de 100%, precisamos garantir que isso não 
+foi sorte de uma divisão específica gerada pelo train_test_split.'''
+
+#.predict_proba() devolve a porcentagem de certeza do modelo para cada classe
+#retorna um array com duas colunas [probabilidade_classe_0(crédito não alto), probabilidade_classe_1(crédito alto)]
+probabilidades = naive_bayes.predict_proba(X_teste)
+prob_credito_alto = probabilidades[:, 1]
+df_resultados = pd.DataFrame({
+    'Valor Real': y_teste.values.ravel(),
+    'Previsão': pred_test,
+    'Certeza Credito Alto (%)': np.round(prob_credito_alto * 100, decimals= 2)})
+print(df_resultados.head(10))
+
+# Juntando treino e teste temporariamente para rodar validação cruzada na base inteira
+X_total = pd.concat([X_treino, X_teste])
+y_total = pd.concat([y_treino, y_teste])
+
+scores_acuracia = cross_val_score(naive_bayes, X_total, y_total.values.ravel(), cv=5, scoring='accuracy')
+
+print(f"\n\nAcurácias obtidas em cada uma das 5 rodadas: {np.round(scores_acuracia * 100, 2)}%")
+print(f"Acurácia Média Final: {scores_acuracia.mean() * 100:.2f}%")
+print(f"Desvio Padrão: {scores_acuracia.std() * 100:.2f}%")
+'''acurácia se manteve alta em todas os folds, prova estatistica da hipótese inicial 
+que o dataset é altamente previsível e não é um caso de overfitting ou leakage'''
