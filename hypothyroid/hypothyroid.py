0a2a4a95a9ca3f2320e905e53ce310ae80ad1acf
Emanuelle Natal <emanuellenmoura@gmail.com>
2026-05-01 23:37:44 -0300

    arvore de decisao como modelo base

--- hypothyroid/hypothyroid.py
+++ hypothyroid/hypothyroid.py
@@ -1,4 +1,7 @@
 from utils.pre_processing import *
+from sklearn.tree import plot_tree, DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import cross_val_score, GridSearchCV
 
 '''
 o objetivo desse modelo é diagnosticar e classificar pacientes
@@ -12,9 +15,12 @@ print(data.df.info()) #só categóricas
 data.to_number(['age', 'TSH', 'T3', 'TT4', 'T4U', 'FTI', 'TBG'])
 data.print_missing()
 data.drop_columns(['TBG', 'TSH measured', 'T3 measured', 'TT4 measured', 'T4U measured',
-                   'FTI measured', 'TBG measured']) #TBG falta tudo, não traz informação e colunas de measured só diz se foi feito exame
+                   'FTI measured', 'TBG measured', 'referral source'])
+#TBG falta tudo, não traz informação e colunas de measured só diz se foi feito exame
+#'referral source' estava causando ruído, dado admnistrativo
 data.df.dropna(subset=['age'], inplace=True) #só 1 linha sem idade, remover ela
-data.imputar_knn(['T3', 'T4U', 'FTI', 'TSH', 'TT4'])  #usa média de 5 pacientes parecidos
+#poucos faltantes ~5%, knn imputer estava causando data leakage(substituia faltantes aprendendo valores da base toda), ao invés de colocar em um pipeline decidi usar simple imputer
+data.imputar_simples(['TSH', 'T3', 'TT4', 'T4U', 'FTI'])
 
 data.print_unique_values()
 #remapeando booleanas
@@ -25,7 +31,8 @@ colunas_booleanas = ['on thyroxine', 'query on thyroxine', 'on antithyroid medic
 data.df[colunas_booleanas] = data.df[colunas_booleanas].replace({'f': 0, 't': 1})
 data.df['binaryClass'] = data.df['binaryClass'].replace({'N': 0, 'P': 1})
 data.df['sex'] = data.df['sex'].replace({'?': np.nan, 'F': 0, 'M': 1}) #150 faltando(~4%)
-data.df['sex'] = data.df['sex'].fillna(df['sex'].mode()[0])
+data.imputar_simples(['sex'], estrategia='most_frequent')
+data.df['binaryClass'] = data.df['binaryClass'].astype(int)
 
 print(data.df.describe().T) #sinais de outlier em 'age', 'TSH', 'TT4', 'FTI'
 data.box_plot_multi(['age', 'TSH', 'TT4', 'FTI'], "Antes de Tratar Outliers")
@@ -33,16 +40,74 @@ data.apply_log(['TSH', 'TT4', 'FTI']) #cauda longa bilateral, log para melhorar 
 data.capping_outliers(['age']) #só 1 valor mt distoante(vampiro de 455 anos)
 data.box_plot_multi(['TSH', 'TT4', 'FTI'], "Depois de Tratar Outliers")
 '''melhor análise visual, caudas longas ainda lá, mas capping evitado para não descaracterizar os exames
-o q faz sentido pela escolha do algoritmo XGBoost que não é tão sensível a outliers e 
+o q faz sentido pela escolha do algoritmo de árvores que não é tão sensível a outliers e 
 não precisam de escalonamento(por isso std scaler não usada)'''
 
 #print(data.df.info())
-dicionario_encoder = data.label_encoding()
-#data.print_missing() #um paciente com todos os exames nulos imputar_knn não conseguiu substituir o valor
-data.df = data.df.dropna()
-data.heatmap()
-'''maioria tem pouca correlação com referral source, 
-psych tem uma relação negativa de -0.40 sendo a que mais influencia'''
-data.separar_base('referral source')
-verificar_base(data.X_train, data.X_test, data.y_train, data.y_test, 'referral source')
-print(f"Análise de Multicolinearidade\n: {data.get_vif()}\n") #TT4, FTI e T4U têm alta redundância
+data.heatmap() #binaryClass têm forte correlação negativa com tsh(-0.69)
+data.separar_base('binaryClass')
+verificar_base(data.X_train, data.X_test, data.y_train, data.y_test, 'binaryClass')
+#base muito desbalanceada, oversampling
+data.smote()
+
+#decision tree como modelo baseline
+'''na primeira tentativa teve muito sobreajuste por isso profundidade limitada'''
+arvore_decisao = DecisionTreeClassifier(criterion='gini', random_state=0, max_depth=4)
+arvore_decisao.fit(data.X_train, data.y_train)
+
+pred_treino = arvore_decisao.predict(data.X_train)
+acuracia_treino = accuracy_score(data.y_train, pred_treino)
+print(classification_report(data.y_train, pred_treino))
+print(f"\nAcurácia do modelo base nos dados de treino: {acuracia_treino * 100:.2f}%\n")
+
+pred_teste = arvore_decisao.predict(data.X_test)
+acuracia_teste = accuracy_score(data.y_test, pred_teste)
+print(classification_report(data.y_test, pred_teste))
+print(f"\nAcurácia do modelo base nos dados de teste: {acuracia_teste * 100:.2f}%\n")
+
+#validação cruzada
+X_total = pd.concat([data.X_train, data.X_test])
+y_total = pd.concat([data.y_train, data.y_test])
+scores = cross_val_score(arvore_decisao, X_total, y_total.values.ravel(), cv=5)
+print(f"Acurácias do modelo base obtidas em cada uma das 5 rodadas: {np.round(scores * 100, 2)}%")
+print(f"Acurácia média: {scores.mean() * 100:.2f}%")
+#modelo generaliza
+
+class_names = ['Saudável', 'Doente']
+plt.figure(figsize=(12, 8))
+plot_tree(arvore_decisao, filled=True, rounded=True,
+          feature_names=data.X_train.columns, class_names=class_names)
+plt.title("Visualização da Árvore de Decisão")
+plt.show()
+
+conf_matrix(data.y_test, pred_teste, class_names)
+profundidade = arvore_decisao.get_depth()
+print(f"A profundidade da árvore é: {profundidade}")
+
+data.feature_importance(arvore_decisao) #só 6 features não nulas, ~96% do resultado depende do exame de tsh
+features_reduzidas = ['TSH', 'on thyroxine', 'TT4', 'FTI', 'thyroid surgery', 'query hypothyroid']
+X_treino_red = data.X_train[features_reduzidas]
+X_teste_red = data.X_test[features_reduzidas]
+
+parametros_grid = {'criterion': ['gini', 'entropy'], 'max_depth': [3, 4, 5],
+                   'min_samples_split': [2, 5, 10], 'min_samples_leaf': [1, 5, 10]}
+
+grid_search = GridSearchCV(DecisionTreeClassifier(random_state=0),
+                           param_grid=parametros_grid, cv=5, scoring='accuracy', n_jobs=-1)
+
+grid_search.fit(X_treino_red, data.y_train)
+melhor_arvore_red = grid_search.best_estimator_
+
+print(f"Melhores Hiperparâmetros:\n{grid_search.best_params_}")
+pred_red = melhor_arvore_red.predict(X_teste_red)
+acuracia_red_teste = accuracy_score(data.y_test, pred_red)
+print(f"Acurácia do modelo base reduzido nos dados de teste: {acuracia_red_teste * 100:.2f}%\n")
+print(classification_report(data.y_test, pred_red))
+
+plt.figure(figsize=(12, 8))
+plot_tree(melhor_arvore_red, filled=True, rounded=True,
+          feature_names=features_reduzidas, class_names=class_names)
+plt.title("Árvore de Decisão apenas com as features principais")
+plt.show()
+conf_matrix(data.y_test, pred_red, class_names)
+#taxa de positivos verdadeiros e falsos verdadeiros igual mais com 15 features a menos, acurácia também
